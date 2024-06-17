add0f13a4d4d5ad167d78c8cc192b0b934dd1020
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-06-17 13:53:47 +0100

    getWeather function added

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function formatCoords(url) {
   const formattedCoords = coords.replace("%2C", ",");
   return formattedCoords;
 }
+
+async function getWeather(location) {
+  let data;
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
+  );
+  if (response.ok) {
+    data = await response.json();
+  } else {
+    // fall back to region if specific location cannot be found
+    response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${window.globalDataLayer.propertySubRegion}`
+    );
+    data = await response.json();
+  }
+  weatherData = {
+    currentTemp: data.current.temp_c,
+    cloudCover: data.current.cloud,
+    windSpeed: data.current.wind_mph,
+    precipitation: data.current.precip_mm,
+    icon: data.current.condition.icon,
+    conditionText: data.current.condition.text,
+  };
+}
