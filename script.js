25456fed14be2ab1d41a2b9aea5f392368c76470
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-06-17 13:52:58 +0100

    format coordinates function added

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const API_KEY = "";
 const pageBody = document.querySelector(".kOBmMQ");
 const coordURL = document.getElementById("propertyViewOnGoogleMaps_image").href;
 let weatherData = {};
+
+function formatCoords(url) {
+  const coords = url.split("destination=").pop();
+  const formattedCoords = coords.replace("%2C", ",");
+  return formattedCoords;
+}
